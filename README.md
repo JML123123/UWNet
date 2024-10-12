455791239881b3135c04ff74530ac29c6c7fbdef
JML123123 <18032869803@163.com>
2024-10-12 13:57:08 +0800

    second commit

--- README.md
+++
@@ -1,1 +0,0 @@
-<img width="468" alt="image" src="https://github.com/user-attachments/assets/8a575629-3fcf-4352-bd17-92907f3c2916">
--- val.py
+++ val.py
@@ -2,7 +2,6 @@ import warnings
 warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
-# 验证参数官方详解链接：https://docs.ultralytics.com/modes/val/#usage-examples:~:text=of%20each%20category-,Arguments%20for%20YOLO%20Model%20Validation,-When%20validating%20YOLO
 
 if __name__ == '__main__':
     model = YOLO('best.pt')
@@ -10,9 +9,6 @@ if __name__ == '__main__':
               split='val',
               imgsz=640,
               batch=8,
-              # iou=0.7,
-              # rect=False,
-              # save_json=True, # if you need to cal coco metrice
               project='runs/val',
               name='exp'
               )
